a4692882e691612d7fc48fe1a7adf8497d117a76
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-02 17:03:04 -0700

    :construction: styles 5 different ways

---
+++ src/components/Background.tsx
@@ -0,0 +1,14 @@
+import React, { PropsWithChildren } from 'react'
+
+type Props = {}
+
+const Background = ({children}: PropsWithChildren<Props>) => {
+  return (
+    <div style={{
+        width: '100vw',
+        height: '100vh',
+    }}>{children}</div>
+  )
+}
+
+export default Background
