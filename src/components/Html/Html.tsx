a4a1288378525139095a9af1775eca1deb6660b0
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-19 14:58:18 -0700

    :hammer: default page layout

---
+++ src/components/Html/Html.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren } from 'react'
+import Fonts from '../Fonts/Fonts';
+import ThemeProvider from '../ThemeProvider/ThemeProvider';
+
+type Props = {
+    // Title of page (passed to `<head>`)
+    title: string;
+}
+
+const Html = ({title, children}: PropsWithChildren<Props>) => {
+  return (
+    <html lang="en">
+        <head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <Fonts />
+            <title>{title}</title>
+        </head>
+        <body>
+            <ThemeProvider>
+                {children}
+            </ThemeProvider>
+        </body>
+    </html>
+  )
+}
+
+export default Html
