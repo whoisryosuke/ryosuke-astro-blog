aba4afd6487ee081430e81b998496fc8e111a290
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-26 16:02:18 -0700

    :sparkles: playlist

---
+++ src/components/Playlist/Playlist.css.ts
@@ -0,0 +1,16 @@
+import { base } from '@/theme/base.css';
+import { style } from '@vanilla-extract/css';
+
+const OPACITY_UNSELECTED = 0.5;
+
+export const playlistCategoryStyles = style({
+    opacity: OPACITY_UNSELECTED,
+    transition: '420ms opacity ease-in',
+    background: 'none',
+    paddingLeft: base.tokens.space[4],
+    paddingRight: base.tokens.space[4],
+
+    ':hover': {
+        opacity: 1,
+    }
+})
---
+++ src/data/playlist.js
@@ -0,0 +1,87 @@
+export const PLAYLIST_CATEGORIES = {
+  chill: "Chill",
+  rock: "Rock",
+};
+
+const PLAYLIST_DATA = [
+  {
+    artist: "NEIKED, Mae Muller, Polo G",
+    song: "Better Days",
+    url: "https://www.youtube.com/watch?v=GQAOrCOknCY&list=PLUGDC1tkjMjcib-qdTJ0ew7aobW47RTbl&index=35",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "LISA",
+    song: "Money",
+    url: "https://www.youtube.com/watch?v=4Dk3YAN7Axc&list=PLUGDC1tkjMjcib-qdTJ0ew7aobW47RTbl&index=37",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "Y2K, bbno$",
+    song: "Lalala",
+    url: "https://www.youtube.com/watch?v=N2Y2vQ-1m7M&list=PLUGDC1tkjMjcib-qdTJ0ew7aobW47RTbl&index=162",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "coldrain",
+    song: "ENVY",
+    url: "https://www.youtube.com/watch?v=2teepAfDrXI&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=2",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "Twenty One Pilots",
+    song: "The Outside",
+    url: "https://www.youtube.com/watch?v=eNcvblM8-_o",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "Erra",
+    song: "Divisionary",
+    url: "https://www.youtube.com/watch?v=rpFl-YYw5BE&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=56",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "Escape the Fate",
+    song: "Lightning Strike",
+    url: "https://www.youtube.com/watch?v=h5xU2bghopw&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=60",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "The Correspondents",
+    song: "Fear & Delight",
+    url: "https://www.youtube.com/watch?v=ABS-mlep5rY&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=87",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "Saint Motel",
+    song: "A Good Song Never Dies",
+    url: "https://www.youtube.com/watch?v=s29fcv5E52Y&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=101",
+    category: PLAYLIST_CATEGORIES.chill,
+  },
+  {
+    artist: "Electric Callboy",
+    song: "We Got the Moves",
+    url: "https://www.youtube.com/watch?v=D1NdGBldg3w&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=163",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "Spiritbox",
+    song: "Circle With Me",
+    url: "https://www.youtube.com/watch?v=zMnfkW9UZc4&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=274",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "Cold Orange",
+    song: "Out For Blood",
+    url: "https://www.youtube.com/watch?v=Toa2nKLgHz8&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=327",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+  {
+    artist: "Bad Omens",
+    song: "Artificial Suicide",
+    url: "https://www.youtube.com/watch?v=IBcUkTXAwbw&list=PLUGDC1tkjMjd-u6hTT-5lYHvbaDpI9Kzu&index=328",
+    category: PLAYLIST_CATEGORIES.rock,
+  },
+];
+
+export default PLAYLIST_DATA;
