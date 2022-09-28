30f3ab782dcd86c1f6b86d8cf2a1685bf647bc51
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-28 13:19:48 -0700

    :sparkles: tag page

---
+++ src/components/Accordion/Accordion.css.ts
@@ -0,0 +1,16 @@
+import { style } from "@vanilla-extract/css";
+import { themes, base } from "../../theme";
+
+export const accordionButtonStyles = style({
+    width:'100%', background: 'none', border:0, textAlign:'left', cursor: 'pointer',
+    ':hover': {
+        color: themes.tokens.colors.primary.default,
+    }
+})
+
+export const accordionBarStyles = style({
+    ':hover': {
+        color: themes.tokens.colors.primary.default,
+    }
+})
+
