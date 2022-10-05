3e9cc739f60da596f539d07fe08e31aeb83c1023
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-10-04 17:25:21 -0700

    :lipstick: better blog container mobile full width

---
+++ src/components/BlogContainer/BlogContainer.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx'
+import React, { PropsWithChildren } from 'react'
+import Box, { BoxProps } from '../Box/Box'
+import { blogContainerStyles } from './BlogContainer.css'
+
+type Props = BoxProps & {}
+
+const ContentContainer = ({children, className, ...props}: PropsWithChildren< Props>) => {
+  return (
+    <Box className={clsx(blogContainerStyles, className)} {...props}>{children}</Box>
+  )
+}
+
+export default ContentContainer
