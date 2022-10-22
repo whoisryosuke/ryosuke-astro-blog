105b991a3686351222629b0a9410c33ffcb5440a
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-10-21 23:22:10 -0700

    :sparkles: added meta + social og

--- src/components/Html/Html.tsx
+++ src/components/Html/Html.tsx
@@ -4,21 +4,28 @@ import ThemeProvider from "../ThemeProvider/ThemeProvider";
 import "normalize.css";
 import { MainNavProvider } from "src/context/MainNavContext";
 import { bodyStyles } from "./Html.css";
+import MetaTags, { MetaTagsProps } from "../MetaTags/MetaTags";
 
 type Props = {
   // Title of page (passed to `<head>`)
   title: string;
+
+  meta: MetaTagsProps;
 };
 
-const Html = ({ title, children }: PropsWithChildren<Props>) => {
+const Html = ({ title, meta, children }: PropsWithChildren<Props>) => {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <Fonts />
         <title>{title}</title>
+
+        {/* Social Media Stuff */}
+        <MetaTags {...meta} />
+
+        <Fonts />
         <script src="/sw.js" />
       </head>
       <body className={bodyStyles}>{children}</body>
---
+++ src/components/MetaTags/MetaTags.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export type MetaTagsProps = {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+}
+
+const MetaTags = ({
+    title = "Ryo's Blog",
+    description = "The blog and portfolio of Ryosuke Hana",
+    image = "/images/social-default.png",
+    url = ""
+}: MetaTagsProps) => {
+  return (
+    <>
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="article" />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://whoisryosuke.com${image}`} />
+      <meta property="og:url" content={`https://whoisryosuke.com${url}`} />
+      <meta name="twitter:card" content="summary_large_image"></meta>
+    </>
+  )
+}
+
+export default MetaTags
