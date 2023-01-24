e9e64212766df2e2c7d4c8ead48922f345544254
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-01-23 17:15:29 -0800

    :lipstick: dont scale imgs + captions

--- src/components/Image/Image.css.ts
+++ src/components/Image/Image.css.ts
@@ -2,5 +2,24 @@ import { style } from "@vanilla-extract/css";
 import { themes, base } from "../../theme";
 
 export const imageStyles = style({
-  width: "100%",
+  width: "auto",
+  margin: "auto",
+});
+
+export const imageContainerStyles = style({
+  width: "100%", 
+  textAlign:'center'
+});
+
+export const captionContainerStyles = style({
+  display: "inline-block",
+  margin: "auto",
+  textAlign:'center',
+  backgroundColor: themes.tokens.colors.glass,
+  marginTop: base.tokens.space[2],
+  marginBottom: base.tokens.space[2],
+  padding: base.tokens.space[1],
+  paddingLeft: base.tokens.space[4],
+  paddingRight: base.tokens.space[4],
+  borderRadius: base.tokens.space[3],
 });
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,13 +1,19 @@
 import clsx from "clsx";
 import React, { AnchorHTMLAttributes } from "react";
-import { imageStyles } from "./Image.css";
+import Box from "../Box/Box";
+import Text from "../Text/Text";
+import { imageStyles,imageContainerStyles, captionContainerStyles } from "./Image.css";
 
 export type ImageProps = AnchorHTMLAttributes<HTMLImageElement> & {
   children: React.ReactNode;
+  alt: string;
 };
 
-const Image = ({ className, ...props }: ImageProps) => {
-  return <img className={clsx(imageStyles, className)} {...props} />;
+const Image = ({ className, alt, ...props }: ImageProps) => {
+  return <Box className={imageContainerStyles}>
+      <img className={clsx(imageStyles, className)} alt={alt} {...props} />
+      {alt && <Box><Box className={captionContainerStyles} aria-hidden><Text>{alt}</Text></Box></Box>}
+  </Box>;
 };
 
 export default Image;
