315db591667617a0006046132c8b0f7afdce9447
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-04-18 15:45:08 -0700

    :lipstick: always white in blockquotes

--- src/components/Text/Text.css.ts
+++ src/components/Text/Text.css.ts
@@ -1,7 +1,15 @@
 import { style } from "@vanilla-extract/css";
 import { themes, base } from "../../theme";
+import { blockquoteStyles } from "../Blockquote/Blockquote.css";
 
 export const textStyles = style({
   fontFamily: "Roboto, Helvetica, Helvetica Neue, Open Sans, Arial, sans-serif",
   color: themes.tokens.colors.text,
+
+  
+  selectors: {
+    [`${blockquoteStyles} &`]: {
+      color: 'rgba(234,234,241,1)',
+    },
+  }
 });
